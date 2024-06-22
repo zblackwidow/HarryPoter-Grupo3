9623cacecc6b23d1c597627b09b5113a096816b6
Andrés Rodas <andresrodas98.ar@gmail.com>
2024-06-21 21:42:10 -0500

    Tabla de hechizos completa con sus filtros funcionales

--- Javascript/spells.js
+++ Javascript/spells.js
@@ -11,8 +11,11 @@ const aplicationSpell = createApp({
             name: [],
             description: [],
             effect: [],
-            category: []
-
+            category: [],
+            search: "",
+            selectOrder: ``,
+            selectSort: ``,
+            spellBk: [],
         }
     },
     created() {
@@ -22,21 +25,50 @@ const aplicationSpell = createApp({
         traerData(url) {
             fetch(url).then(response => response.json()).then(datas => {
                 console.log(datas);
-                let {data} = datas
+                let { data } = datas
                 console.log(data);
 
                 this.image = data.map((e) => e.attributes.image)
-                this.name = data.map((e) => e.attributes.name) 
-                this.description = data.map((e) => e.attributes.description) 
-                this.effect = data.map((e) => e.attributes.effect) 
+                this.name = data.map((e) => e.attributes.name)
+                this.description = data.map((e) => e.attributes.description)
+                this.effect = data.map((e) => e.attributes.effect)
                 this.category = Array.from(new Set(data.map((e) => e.attributes.category)))
                 this.spell = data
-
-                console.log(this.spell);              
+                this.spellBk = data
+                console.log(this.spell);
             })
         }
     },
     computed: {
-      
+        filter() {
+            let filterText = this.spellBk.filter(e => e.attributes.name.toLowerCase().includes(this.search.toLowerCase().trim()))
+            this.spell = filterText
+
+            let nameDesc = this.name.sort((a, b) => b.localeCompare(a))
+            console.log(nameDesc);
+
+            let sorts = this.selectSort
+            let orders = this.selectOrder
+
+            if (sorts === "name" && orders === "falling") {
+                let nameDesc = this.spell.sort((a, b) => b.attributes.name.localeCompare(a.attributes.category))
+                this.spell = nameDesc
+            }
+
+            if (sorts === "name" && orders === "upward") {
+                let nameAsce = this.spell.sort((a, b) => a.attributes.name.localeCompare(b.attributes.name))
+                this.spell = nameAsce
+            }
+
+            if (sorts === "category" && orders === "upward") {
+                let categoryAsc = this.spell.sort((a, b) => a.attributes.category.localeCompare(b.attributes.category))
+                this.spell = categoryAsc
+            }
+
+            if (sorts === "category" && orders === "falling") {
+                let categoryDes = this.spell.sort((a, b) => b.attributes.category.localeCompare(a.attributes.category))
+                this.spell = categoryDes
+            }
+        }
     }
 }).mount('#aplicationSpell')
