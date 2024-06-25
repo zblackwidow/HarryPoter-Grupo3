b6880b8c8a8b488ef9588053844db0b285b12d82
blackwidow <alejandrazacariasrt@gmail.com>
2024-06-25 11:30:29 -0300

    listo el main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,71 +3,42 @@ let url = "https://potterhead-api.vercel.app/api/characters";
 
 document.querySelectorAll('.cardcasa').forEach(card => {
     card.addEventListener('click', () => {
-        const House = card.id;
-        window.location.href = `/page/detailsHouses.html?House=${House}`;
+        const houseId = card.id;
+        window.location.href = `/page/detailsHouses.html?houseId=${houseId}`;
     });
 });
-function newUrl(param) {
+function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
 }
 
-const House = newUrl('House');
+const houseId = getQueryParam('houseId');
 
-function houseDetails (House) {
+function HouseDetails(houseId) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
             const characterList = document.getElementById('characterList');
-            const filteredCharacters = data.filter(character => character.house === House);
+            const filteredCharacters = data.filter(character => character.house === houseId);
 
+            
             if (filteredCharacters.length > 0) {
                 filteredCharacters.forEach(student => {
-                    const studentCard = document.createElement('div');
-                    studentCard.innerHTML = `
+                    const characterCard = document.createElement('div');
+                    characterCard.innerHTML = `
                         <div class="carddetails p-2 m-3">
-                        <a href="./details.html"> 
-                        <img class="studentImg w-100 object-fit-cover" src="${student.image}"  alt="${character.name}">
+                        <a href="/page/details.html?value=${student.id}"> 
+                        <img class="studentImg w-100 object-fit-cover" src="${student.image}"  alt="${student.name}">
                         </a>   
                         </div>
                     `;
                     characterList.appendChild(characterCard);
                 });
             } else {
-                characterList.innerHTML = '<p>No se encontraron personajes para esta casa.</p>';
+                characterList.innerHTML = '<p>No characters found for this house.</p>';
             }
         })
 }
 
 
-houseDetails (House);
-
-// let characters = [];
-
-// function obtenerPersonajes() {
-//     fetch(charactersApiUrl)
-//         .then(response => response.json())
-//         .then(data => {
-//             characters = data;
-//             let gryffindorCharacters = characters.filter(character => character.house === "Gryffindor");
-//             mostrarPersonajes(gryffindorCharacters);
-//         })
-// }
-
-// function mostrarPersonajes(personajes) {
-//     let cardContainer = document.getElementById('card');
-//     cardContainer.innerHTML = '';
-//     personajes.forEach(personaje => {
-//         let cardContenedor = `
-//         <div class="carddetails p-2 m-3">
-//            <a href="./details.html"> 
-//            <img class="studentImg w-100 object-fit-cover" src="${personaje.image}"  alt="${personaje.name}">
-//            </a>   
-//         </div>
-        
-//         `;
-//         cardContainer.innerHTML += cardContenedor;
-//     });
-// }
-
-// obtenerPersonajes();
+HouseDetails(houseId);
