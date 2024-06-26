05480dcdb5eaffc8680431c1c8f8a888a9ef0a9d
blackwidow <alejandrazacariasrt@gmail.com>
2024-06-25 23:35:30 -0300

    coño

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,45 +1,78 @@
-let url = "https://potterhead-api.vercel.app/api/characters";
+const { createApp } = Vue;
 
-document.querySelectorAll('.cardcasa').forEach(card => {
-    card.addEventListener('click', () => {
-        const houseId = card.id;
-        window.location.href = `/page/detailsHouses.html?houseId=${houseId}`;
-    });
+const app = createApp({
+    data() {
+        return {
+            url: "https://potterhead-api.vercel.app/api/characters",
+            houseId: new URL(window.location.href).searchParams.get("houseId"),
+            characters: [],
+            houseName: ''
+        }
+    },
+    created() {
+        this.fetchHouseDetails();
+    },
+    methods: {
+        fetchHouseDetails() {
+            fetch(this.url)
+                .then(response => response.json())
+                .then(data => {
+                    const filteredCharacters = data.filter(character => character.house === this.houseId);
+                    if (filteredCharacters.length > 0) {
+                        this.houseName = this.houseId;
+                        this.characters = filteredCharacters;
+                    } else {
+                        console.log("No characters found for this house.");
+                    }
+                });
+        }
+    }
 });
 
-function newID(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
+app.mount('#app');
 
-const houseId = newID('houseId');
+// let url = "https://potterhead-api.vercel.app/api/characters";
 
-function HouseDetails(houseId) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const characterList = document.getElementById('characterList');
-            const houseNameElement = document.getElementById('houseName');
-            const filteredCharacters = data.filter(character => character.house === houseId);
+// document.querySelectorAll('.cardcasa').forEach(card => {
+//     card.addEventListener('click', () => {
+//         const houseId = card.id;
+//         window.location.href = `/page/detailsHouses.html?houseId=${houseId}`;
+//     });
+// });
 
-            if (filteredCharacters.length > 0) {
-                houseNameElement.textContent = houseId;
+// function newID(param) {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     return urlParams.get(param);
+// }
 
-                filteredCharacters.forEach(student => {
-                    const studentImage = student.image || '/img/undefinedStudent.png';
-                    const characterCard = document.createElement('div');
-                    characterCard.innerHTML = `
-                    <div class="tarjetaDetails m-3"><a href="/page/details.html?id=${student.id}">
-                    <img src="${studentImage}" class="img w-100 h-100 object-fit-cover">
-                    <div class="profile-name"> ${student.name}</a></div>
-                     </div>
-                    `;
-                    characterList.appendChild(characterCard);
-                });
-            } else {
-                console.log("se rompió");
-            }
-        });
-}
+// const houseId = newID('houseId');
 
-HouseDetails(houseId);
+// function HouseDetails(houseId) {
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             const characterList = document.getElementById('characterList');
+//             const houseNameElement = document.getElementById('houseName');
+//             const filteredCharacters = data.filter(character => character.house === houseId);
+
+//             if (filteredCharacters.length > 0) {
+//                 houseNameElement.textContent = houseId;
+
+//                 filteredCharacters.forEach(student => {
+//                     const studentImage = student.image || '/img/undefinedStudent.png';
+//                     const characterCard = document.createElement('div');
+//                     characterCard.innerHTML = `
+//                     <div class="tarjetaDetails m-3"><a href="/page/details.html?id=${student.id}">
+//                     <img src="${studentImage}" class="img w-100 h-100 object-fit-cover">
+//                     <div class="profile-name"> ${student.name}</a></div>
+//                      </div>
+//                     `;
+//                     characterList.appendChild(characterCard);
+//                 });
+//             } else {
+//                 console.log("se rompió");
+//             }
+//         });
+// }
+
+// HouseDetails(houseId);
